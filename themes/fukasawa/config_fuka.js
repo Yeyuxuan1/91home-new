9498a3ace59ab2fc27b73f9c9c42f18ec4e5e7bb
Yeyuxuan1 <130195420+Yeyuxuan1@users.noreply.github.com>
2023-06-10 08:34:16 +0800

    Update config_fuka.js

--- themes/fukasawa/config_fuka.js
+++ themes/fukasawa/config_fuka.js
@@ -1,11 +1,11 @@
 const FUKA_CONFIG = {
 
-  POST_LIST_COVER: true, // 文章列表显示图片封面
+  POST_LIST_COVER: false, // 文章列表显示图片封面
   POST_LIST_PREVIEW: false, // 显示文章预览
 
   // 菜单
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: false // 显示搜索
 
